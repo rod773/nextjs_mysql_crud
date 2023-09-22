612ccdb7a8011d5830c19014ca34a3cd3d13da3d
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-22 19:47:15 +0200

    new page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,1 @@
-function about() {
-  return <h1 className="text-3xl font-bold underline">About</h1>;
-}
 
-export default about;
---
+++ pages/new.js
@@ -0,0 +1,5 @@
+function NewPage() {
+  return <h1 className="text-3xl font-bold underline">Add One Product</h1>;
+}
+
+export default NewPage;
