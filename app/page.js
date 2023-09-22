fa6169510eeaf70b1037b04f4d47001ed672a7fc
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-22 17:48:30 +0200

    instalacion de tailwind

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>nextjs mysql crud</h1>
+      <h1 className="text-3xl font-bold underline">Nextjs Mysql Crud</h1>
     </div>
   );
 }
