c31f9e10c02f396cd73bb0fa30514746455fa51c
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-23 14:00:54 +0200

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+function about() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">About Page</h1>
+    </div>
+  );
+}
+
+export default about;
